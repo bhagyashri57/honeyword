c381c74ddfa37956a9d81d65477b522495e7113b
Bhagyashri Dhanaraj Suryawanshi <bhagyashrisurya57@gmail.com>
2017-01-24 17:13:30 +0530

    if2.py is added

---
+++ if2.py
@@ -0,0 +1,12 @@
+a = int(input("Enter number"))
+b= int(input("Enter number"))
+c=((a+b)/200)*100
+
+if(a>70):
+    print("Dist")
+
+elif (a>60):
+    print("1st class")
+
+else:
+    print("2nd class")
